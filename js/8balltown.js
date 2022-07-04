450cacd1086ac2d0fc6935f4f9fcb01e16247fb1
uj1107 <uj1107@naver.com>
2022-07-04 19:02:20 +0900

    first commit

---
+++ js/8balltown.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+  document.getElementById("vid-all").onclick = function () {
+    $("ul.videos").show();
+  };
+  document.getElementById("vid-kirin").onclick = function () {
+    $("ul.videos").hide();
+    $(".kirin").show();
+  };
+  document.getElementById("vid-jgr").onclick = function () {
+    $("ul.videos").hide();
+    $(".jgr").show();
+  };
+  document.getElementById("vid-plastic-kid").onclick = function () {
+    $("ul.videos").hide();
+    $(".plastic-kid").show();
+  };
+  document.getElementById("vid-meenoi").onclick = function () {
+    $("ul.videos").hide();
+    $(".meenoi").show();
+  };
+  document.getElementById("vid-jason").onclick = function () {
+    $("ul.videos").hide();
+    $(".jason").show();
+  };
+  document.getElementById("vid-yunu").onclick = function () {
+    $("ul.videos").hide();
+    $(".yunu").show();
+  };
+  document.getElementById("vid-bronze").onclick = function () {
+    $("ul.videos").hide();
+    $(".bronze").show();
+  };
+  document.getElementById("vid-jeyon").onclick = function () {
+    $("ul.videos").hide();
+    $(".jeyon").show();
+  };
+  document.getElementById("vid-yoyo").onclick = function () {
+    $("ul.videos").hide();
+    $(".yoyo").show();
+  };
+  document.getElementById("vid-sindogg").onclick = function () {
+    $("ul.videos").hide();
+    $(".sindogg").show();
+  };
+  document.getElementById("vid-mogwa").onclick = function () {
+    $("ul.videos").hide();
+    $(".mogwa").show();
+  };
+  document.getElementById("vid-zoorumpug").onclick = function () {
+    $("ul.videos").hide();
+    $(".zoorumpug").show();
+  };
+  document.getElementById("vid-monocat").onclick = function () {
+    $("ul.videos").hide();
+    $(".monocat").show();
+  };
+  document.getElementById("vid-hookuo").onclick = function () {
+    $("ul.videos").hide();
+    $(".hookuo").show();
+  };
+  document.getElementById("vid-light").onclick = function () {
+    $("ul.videos").hide();
+    $(".light").show();
+  };
+
+  // function category(clicked_id) {
+  //   let str = clicked_id;
+  //   let sub = str.substring(4);
+
+  //   $("ul.videos").hide();
+  //   $("ul." + sub);
+  // }
+});
